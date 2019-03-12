610da5eba4e2cd03347930bb00aa6fe5b4d0edac
wangzhen <wangzhen@mistong.com>
2019-03-12 19:49:08 +0800

    热部署

--- src/main/java/com/example/demo/GetController.java
+++ src/main/java/com/example/demo/GetController.java
@@ -1,4 +1,4 @@
-package com.example.demo.filter;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +10,6 @@ public class GetController {
     @GetMapping("/hello")
     @ResponseBody/**/
     String home(){
-        return "hello world";
+        return "hello world 4";
     }
 }
--- src/main/java/com/example/demo/filter/LoginFilter.java
+++ src/main/java/com/example/demo/filter/LoginFilter.java
@@ -32,6 +32,6 @@ public class LoginFilter implements Filter {
 
     @Override
     public void destroy() {
-        System.out.println("destroy");
+        System.out.println("destroy2");
     }
 }
